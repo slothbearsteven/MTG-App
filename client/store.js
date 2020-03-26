8ccbbfc74dc0e0411e34fd7c2b37a9f0ac8f5119
Steven P. Kennedy <umbrylax@gmail.com>
2020-03-26 08:49:27 -0600

    Store being set up

--- client/store.js
+++ client/store.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    players: [{ name: "player1", life: 20, counters: 0 }, { name: "player2", life: 20, counters: 0 }],
+    cards: [],
+    currentCard: {}
   },
   mutations: {
+    setPlayer(state, payload) {
+      state.players[payload.x] = payload.player
+    },
+    setCards(state, cards) {
+      state.cards = cards
+    },
+    setCurrentCard(state, card) {
+      state.currentCard = card
+    }
   },
   actions: {
   },
