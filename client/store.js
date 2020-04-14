10cc8d2e21b4503561f2f30e835253abb63389b2
Steven P. Kennedy <umbrylax@gmail.com>
2020-04-13 19:55:11 -0600

    adjust life action added

--- client/store.js
+++ client/store.js
@@ -5,13 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    players: [{ name: "player1", life: 20, counters: 0 }, { name: "player2", life: 20, counters: 0 }],
+    players: [{ number: 0, name: "player1", life: 20, counters: 0 }, { number: 1, name: "player2", life: 20, counters: 0 }],
     cards: [],
     currentCard: {}
   },
   mutations: {
     setPlayer(state, payload) {
-      state.players[payload.x] = payload.player
+      state.players[payload.number] = payload.player
     },
     setCards(state, cards) {
       state.cards = cards
@@ -21,6 +21,19 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    adjustLife({ commit, dispatch }, payload) {
+      switch (payload.change) {
+        case "up": payload.player.life += 1
+          commit("setPlayer", payload)
+          break;
+        case "down": payload.player.life -= 1
+          commit("setPlayer", payload)
+          break;
+
+        default:
+          break;
+      }
+    },
   },
   modules: {
   }
