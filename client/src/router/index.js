068ff9eb26ed00cfd936edcdeac2c6ca850c196e
Steven P. Kennedy <umbrylax@gmail.com>
2020-05-13 08:54:33 -0600

    Made whoopsie and fixed it

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// @ts-ignore
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -17,6 +18,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
+      // @ts-ignore
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
   }
