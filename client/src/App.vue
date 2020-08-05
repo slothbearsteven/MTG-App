52161b5305aad2fbd3d1a5514fcb0d494e8bec5a
Steven P. Kennedy <umbrylax@gmail.com>
2020-08-05 08:25:11 -0600

    Created router link to allow easy acces to search

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>|
+      <router-link to="/search">Search</router-link>|
       <router-link to="/about">About</router-link>
     </div>Beary Magical: Life Counter and Search
     <router-view />
