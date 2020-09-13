03f161679d0e26d31909cb0e24c417f1f3420a3e
Steven P. Kennedy <umbrylax@gmail.com>
2020-09-13 16:58:16 -0600

    added view for single card

---
+++ client/src/views/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card"></div>
+</template>
+
+
+<script>
+export default {
+  name: "card",
+  data() {
+    return {};
+  },
+  computed: {
+    card() {
+      return this.$store.card;
+    },
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
